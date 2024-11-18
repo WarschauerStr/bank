7c704af2a236e91bcb16786c41e697c011f86fa4
max <rokochyi.maksym@gmail.com>
2024-11-18 13:23:15 +0100

    changed login/money operations

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ folder = os.path.expanduser('~/projects/all/banking_system')
 filename = "customers.json"
 
 
+customer = Bank.login(folder, filename)
+if customer == "Error":
+    print("Invalid email or password. Please try again.")
+else:
+    while True:
+        bank = Bank(customer)
+        print(
+            f"\nWelcome back, {bank.fullname}!\n"
+            f"Current balance: ${bank.balance}\n"
+        )
+
+
 # Registrate new customer
 # Bank.register(folder, filename)
 
 # Login
 # Bank.login(folder, filename)
-
-# Deposit money
-# Bank.deposit(1000)
